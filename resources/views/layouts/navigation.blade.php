de9f0d21807d2706c6ca73ac2577caeb16963ea2
brncmn <0309723@student.rocvantwente.nl>
2025-03-04 10:19:10 +0100

    Alligned the code right

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,10 +5,10 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-    <a href="{{ route('dashboard') }}">
-        <img src="{{ asset('images/managementlogo.svg') }}" alt="Management Logo" class="block h-9 w-auto" />
-    </a>
-</div>
+                    <a href="{{ route('dashboard') }}">
+                        <img src="{{ asset('images/managementlogo.svg') }}" alt="Management Logo" class="block h-9 w-auto" />
+                    </a>
+                </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
