5b8a1d2b3d86fb219f63befb079d3232bb4061cf
brncmn <0309723@student.rocvantwente.nl>
2025-03-04 10:16:46 +0100

    Added a nice logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,10 +5,10 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
-                    </a>
-                </div>
+    <a href="{{ route('dashboard') }}">
+        <img src="{{ asset('images/managementlogo.svg') }}" alt="Management Logo" class="block h-9 w-auto" />
+    </a>
+</div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
